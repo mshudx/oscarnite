cbcca10c44ae51a02905b94ac0a61d0b1a7b91e3
Balint Farkas <balint.farkas@windowslive.com>
2016-02-26 14:26:03 +0100

    Added voting functionality (no design yet)

---
+++ src/Mshudx.OscarNite.Web/ViewModels/VotingViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Mshudx.OscarNite.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mshudx.OscarNite.Web.ViewModels
+{
+    public class VotingViewModel
+    {
+        public List<Question> Questions { get; set; }
+        public List<Option> Options { get; set; }
+    }
+}
